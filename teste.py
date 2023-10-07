eec2250a7e86780905c6efed22e5347f510ca9c9
Joao <jvictorpsouza@gmail.com>
2023-10-07 04:27:35 -0300

    .

--- teste.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# Criando o dataframe com duas colunas
-df = pd.DataFrame({'coluna1': [1, 2, 2], 'coluna2': ['a', 'b', 'c']})
-
-# Imprimindo o dataframe
-print(df.describe())
